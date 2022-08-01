0fc7f632d4861c08200964b2da0763e90fe150ee
Luis H <luis.gonzales@oatobservatorio.com>
2022-08-01 21:37:55 +0200

    arreglo llamada api

--- src/componentes/ArrastrarFoto.js
+++ src/componentes/ArrastrarFoto.js
@@ -4,72 +4,46 @@ import API from './APIS/api';
 import Spinner from "react-spinkit";
 
 function ArrastrarFoto() {
-  const [ImageSelectedPrevious, setImageSelectedPrevious] = useState(null);
-  const changeImage = (e) => {
-    console.log(e.target.files);
-    if (e.target.files[0] !== undefined) {
-      const reader = new FileReader();
-      reader.readAsDataURL(e.target.files[0]);
-      reader.onload = (e) => {
-        e.preventDefault();
-        setImageSelectedPrevious(e.target.result); 
 
-      };
-    }
-  };
+const [form_data, set_form_data] = useState();
 
-  const manejarEnvio = e => {
+const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('subiendo...');
-    API.post()
-    .then(response => {
-      if (response.status === 200){
-        alert('subido');
-      }else{
-        alert('errooor de subida');
-      }
+    await API.post('/a', form_data)
+}
 
-    })
-    .catch(error => {
-      alert('error de subida');
-    });
-
-};
-
+const send_image = (files) => {
+    const formData = new FormData();
+    formData.append('file', files)
+    set_form_data(formData)
+    console.log(formData)
+}
 
   return (
     <div>
+              <form onSubmit={handleSubmit}>
+
         <br />
         <div className="image-upload-wrap">
+
           <input
             className="file-upload-input"
             type="file"
             accept="image/*"
             multiple
             onChange={(e) => {
-              changeImage(e);
-            }}
+              send_image(e.target.files[0])
+          }}
           />
           <div className="text-information">
             <h3 className="arrastre">Arrastre o seleccione la imagen que desea compartir</h3>
           </div>
         </div>
+        <button>Save Image</button>
 
-        <div className="center">
-          <img
-            src={ImageSelectedPrevious}
-            alt=""
-            height="150px"
-            width="250px"
-          />
-        </div>
-        <div className="center">
-          <form className="tarea-formulario"
-          onSubmit={manejarEnvio}>
-                <button className="tarea-boton">Subir Imagen</button>
-          </form>
-        </div>
+        </form>
     </div>
+    
   );
 }
 
