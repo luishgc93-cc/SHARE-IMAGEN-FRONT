ed85f623ab0788da7a1517b0352cc160e85b12a9
lhomero <lhomero@ces.vocento.com>
2022-08-05 10:07:43 +0200

    añado registro de datos para el usuario actual

--- src/componentes/UserView.js
+++ src/componentes/UserView.js
@@ -1,13 +1,22 @@
 import React from "react";
 import firebaseApp from "../firebase/credenciales";
-import { getAuth, signOut } from "firebase/auth";
+import { getAuth, signOut, updateProfile,deleteUser } from "firebase/auth";
 import '../estilos/UserView.css';
-
+import { getFirestore, doc, collection, setDoc } from "firebase/firestore";
 const auth = getAuth(firebaseApp);
 
 function UserView(userData) {
-  console.log(userData)
 const email = userData.user.email;
+const firestore = getFirestore(firebaseApp);
+
+  update(userData);
+
+  function update(currentUser) {
+    console.log(currentUser)
+
+    const docuRef = doc(firestore, `usuarios/${currentUser.user.uid}`);
+    setDoc(docuRef, { photoURL: 'zzzz', rol: 'vipss' });
+  }
 
   return(
     <>
